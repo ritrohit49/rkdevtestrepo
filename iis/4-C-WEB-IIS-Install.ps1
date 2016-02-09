6f359e8116ea132b161df611889ba347b9cc654e
ritrohit49 <ritrohit49@gmail.com>
2016-02-09 19:43:56 +0530

    Update 4-C-WEB-IIS-Install.ps1

--- iis/4-C-WEB-IIS-Install.ps1
+++ iis/4-C-WEB-IIS-Install.ps1
@@ -1,6 +1,1 @@
-﻿$winRmUri = Get-AzureWinRMUri -ServiceName vmtwo -Name vmtwo 
-Invoke-Command -ConnectionUri $winRmUri.ToString() -Credential vmtwo\dcadmin -ScriptBlock { 
-    Import-Module ServerManager 
-    Add-WindowsFeature -Name Web-Server -IncludeManagementTools -IncludeAllSubFeature
-    } -Verbose
-Wait-Job -State Completed
+Install windowsFeature -Name vmone
